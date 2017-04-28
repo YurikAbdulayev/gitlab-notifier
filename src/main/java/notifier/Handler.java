dcff0005ba11f08e50f191fd9abf1f2abfd3a84c
Abdulaiev Yurii <yurs11k89@gmail.com>
2017-04-28 21:35:29 +0300

    add environments, add functionality to send message to Slack.

--- src/main/java/notifier/Handler.java
+++ src/main/java/notifier/Handler.java
@@ -1,13 +1,63 @@
 package notifier;
 
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import spark.Request;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.SparkBase.port;
 
 /**
  * Created by yurii on 28.04.17.
  */
 public class Handler {
 
-    public static void main(String[] args) {
+    void watch() {
+        port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello World");
+        get("/msg", (request, response) -> parseRequestData(request));
+        post("msg", (request, response) -> parseRequestData(request));
+    }
+
+    private String parseRequestData(Request request) {
+        System.out.println("ok");
+        return sendMsgToSlack();
+    }
+
+
+    private String sendMsgToSlack() {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            HttpPost request = new HttpPost(getEnv("SLACK_URL"));
+            StringEntity params = new StringEntity(getEnv("PAYLOAD"));
+            request.addHeader("content-type", "application/x-www-form-urlencoded");
+            request.setEntity(params);
+            httpClient.execute(request);
+
+            return "{\"status\": \"ok\"}";
+        } catch (Exception ex) {
+
+            return "{\"status\": " + ex + "}";
+        }
+    }
+
+    private int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4575;
+    }
+
+    private String getEnv(String envName) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get(envName) != null) {
+            return processBuilder.environment().get(envName);
+        }
+        return "";
     }
 }
--- src/main/java/notifier/StartServer.java
+++ src/main/java/notifier/StartServer.java
@@ -4,4 +4,8 @@ package notifier;
  * Created by yurii on 28.04.17.
  */
 public class StartServer {
+
+    public static void main(String[] args) {
+        new Handler().watch();
+    }
 }
